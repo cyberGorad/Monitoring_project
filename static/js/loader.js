8299e0026a2933ee10b91e9253fe9a20f8afccac
root <root@localhost.localdomain>
2025-04-29 11:25:43 +0300

    loader matrix

---
+++ static/js/loader.js
@@ -0,0 +1,41 @@
+const terminals = document.getElementById("terminalText");
+const messages = [
+  "[✓] Initializing system...",
+  "[✓] Connecting to server...",
+  "[✓] Server connected successfully...",
+  "[✓] Loading Data...",
+  "[✓] Alls data Ready is Ready",
+  "[✓] Launching Cybergorad System..."
+];
+
+let indexe = 0;
+let charIndex = 0;
+let currentLine = "";
+
+function typeWriter() {
+  if (indexe < messages.length) {
+    if (charIndex < messages[indexe].length) {
+      currentLine += messages[indexe].charAt(charIndex);
+      terminals.textContent = messages.slice(0, indexe).join("\n") + "\n" + currentLine;
+      charIndex++;
+      setTimeout(typeWriter, 30); // Délai entre chaque caractère
+    } else {
+      // Fin de la ligne actuelle, passer à la suivante
+      terminals.textContent += "\n";
+      indexe++;
+      charIndex = 0;
+      currentLine = "";
+      setTimeout(typeWriter, 500); // Délai avant de passer à la ligne suivante
+    }
+  } else {
+    // Fin du loader après 5 secondes
+    setTimeout(() => {
+      document.getElementById("loader").style.display = "none";
+    }, 5000);
+  }
+}
+
+// Lancer le loader
+window.addEventListener("load", () => {
+  typeWriter();
+});
--- static/js/welcome.js
+++ static/js/welcome.js
@@ -2,10 +2,8 @@
     // Attendre 3 secondes avant de masquer le loader
     window.onload = function() {
         setTimeout(function() {
-            // Cacher le loader après 3 secondes
-            document.getElementById('loader').style.display = 'none';
 
 
             playAudio('/static/bonjour.mp3');
-        }, 3000); // Durée du loader en millisecondes (3 secondes)
+        }, 5000); // Durée du loader en millisecondes (3 secondes)
     };
